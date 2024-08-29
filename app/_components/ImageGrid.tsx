99529d832fec106f460eed7352806307e5765b5b
Gaël Duclocher-Rougelot <gaelduclocher.rougelot@gmail.com>
2024-08-29 18:37:21 +0200

    feat: edit imageGrid

--- app/_components/ImageGrid.tsx
+++ app/_components/ImageGrid.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { HomeDocumentDataImageGalleryItem, Simplify } from "@/prismicio-types";
+"use client";
+import { HomeDocumentDataImageGridItem, Simplify } from "@/prismicio-types";
 import { GroupField } from "@prismicio/client";
 import { motion } from "framer-motion";
 import Image from "next/image";
@@ -7,7 +7,7 @@ import Image from "next/image";
 export default function ImageGrid({
   images,
 }: {
-  images: GroupField<Simplify<HomeDocumentDataImageGalleryItem>>;
+  images: GroupField<Simplify<HomeDocumentDataImageGridItem>>;
 }) {
   return (
     <section className="w-full h-full flex items-center justify-center p-8 md:p-16 max-w-[1420px]">
@@ -32,32 +32,34 @@ export default function ImageGrid({
             />
             <div className="absolute w-full h-full top-0 left-0 p-2">
               <div className="group-over w-full h-full flex flex-col items-center justify-center opacity-0 md:group-hover:opacity-100 bg-white transition-all duration-500 p-10 gap-6 cursor-pointer overflow-hidden">
-                <Image
-                  src={"/map-icon.jpg"}
-                  alt="map icon"
-                  width={100}
-                  height={100}
-                  quality={100}
-                />
-                <p className="text-center text-lg text-ellipsis">
-                  KUALOA RANCH WEDDING
-                </p>
+                {img.icon.url && (
+                  <Image
+                    src={img.icon.url}
+                    alt="map icon"
+                    width={100}
+                    height={100}
+                    quality={100}
+                  />
+                )}
+
+                <p className="text-center text-lg text-ellipsis">{img.title}</p>
                 <button className="text-white bg-[#6b6764] py-2 px-8 text-sm tracking-[0.2rem] border-none">
                   READ MORE
                 </button>
               </div>
             </div>
             <div className="md:hidden w-full flex flex-col items-center justify-center bg-white p-10 gap-6 overflow-hidden">
-              <Image
-                src={"/map-icon.jpg"}
-                alt="map icon"
-                width={100}
-                height={100}
-                quality={100}
-              />
-              <p className="text-center text-lg text-ellipsis">
-                KUALOA RANCH WEDDING
-              </p>
+              {img.icon.url && (
+                <Image
+                  src={img.icon.url}
+                  alt="map icon"
+                  width={100}
+                  height={100}
+                  quality={100}
+                />
+              )}
+
+              <p className="text-center text-lg text-ellipsis">{img.title}</p>
               <button className="text-white bg-[#6b6764] py-2 px-8 text-sm tracking-[0.2rem] border-none">
                 READ MORE
               </button>
--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,14 @@ export default async function Home() {
   const page = await client.getSingle("home");
   const images = page.data.image_gallery;
   const categories = page.data.categories;
-
+  const imagesGrid = page.data.image_grid;
   return (
     <>
       <Header />
       <Hero images={images} />
       <main className="flex min-h-screen flex-col items-center justify-between">
         <About />
-        <ImageGrid images={images} />
+        <ImageGrid images={imagesGrid} />
         <Categories categories={categories} />
       </main>
       <Footer />
