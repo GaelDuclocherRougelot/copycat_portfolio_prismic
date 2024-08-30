27e84793c3c369e2ca58b84fa88260b54f58b589
Gaël Duclocher-Rougelot <gaelduclocher.rougelot@gmail.com>
2024-08-30 15:46:57 +0200

    feat: add burger menu dropdowns

--- app/_components/Dropdown.tsx
+++ app/_components/Dropdown.tsx
@@ -9,15 +9,15 @@ export default function Dropdown({ links }: { links: Array<string> }) {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
-      className="absolute left-1/2 transform -translate-x-1/2 w-fit h-fit bg-slate-50 mt-3 z-50 px-[5vw] py-10 grid place-content-center"
+      className="md:absolute md:left-1/2 md:transform md:-translate-x-1/2 md:w-fit md:h-fit bg-white md:bg-gray-50 mt-3 z-50 px-[5vw] py-10 md:grid md:place-content-center"
     >
-      <div className="w-4 h-4 bg-slate-50 absolute z-40 -top-1 rotate-45 left-1/2 transform -translate-x-1/2"></div>
+      <div className="hidden md:block w-4 h-4 md:bg-gray-50 absolute z-40 -top-1 rotate-45 left-1/2 transform -translate-x-1/2"></div>
       <ul className="flex flex-col gap-6">
         {links &&
           links.map((link, index) => (
             <Link
               href={link.toLowerCase()}
-              className="text-grayCaption text-nowrap md:text-[1vw]"
+              className="text-grayCaption text-nowrap text-sm md:text-[1vw]"
             >
               {link}
             </Link>
